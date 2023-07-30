ee9879367c8c7c69085039eb24f3dfff4628f48b
Priyanshu <priyanshukumar0987@gmail.com>
2023-07-30 16:49:44 +0530

    skipped staging area

--- text2.cpp
+++ text2.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main () {
-    for (int i = 0; i < 10; i++)
-    {
+    for (int i = 0; i < 10; i++) {
         cout<<i<<" ";
     }
-    
-
+    // 3 increase
+    // git diff = working tree compare staging area
+    // git diff --staged = staging area compare last commit
     return 0;
 }
