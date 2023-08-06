e9b9bef0978cf0fe91e26af39bf407b48bc86f37
Priyanshu <priyanshukumar0987@gmail.com>
2023-08-06 18:46:35 +0530

    Tales of a sort

--- Tales of a sort.cpp
+++ Tales of a sort.cpp
@@ -1,22 +1,17 @@
 // <!-- This code is wriiten by Priyanshu -->
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
 
 void solve() {
     int n; cin>>n;
-    vector<int>a(n);
-    for(int i=0; i<n; i++)cin>>a[i];
-    int x=a[n-1];
-    for(int i=n-1; i>0; i--){
-        if(a[i]<a[i-1])break;
-        else if(a[i]>=a[i-1])x=i;
+    int arr[n];
+    for(int i=0;i<n;i++)cin>>arr[i];
+    int x=0;
+    for(int i=0;i<n-1;i++){
+        if(arr[i]>arr[i+1]) x=max(x,arr[i]);
     }
-    sort(a.begin(), a.begin()+x);
-    if(x==1)cout<<0;
-    else cout<<a[x-1];
-    cout<<"\n";
+    cout<<x<<"\n";
 }
 
 int main () {
